e7b4b103136b34efc15c86bb7af87003bd9a67d1
Graham Kelly <graham.kelly@mttnow.com>
2016-07-29 15:15:38 +0100

    Send multiple message to the queue and retrieve in multiple consumer threads. Based on Camel threading

--- CamelDemo.groovy
+++ CamelDemo.groovy
@@ -1,21 +1,96 @@
 @Grab(group = "org.apache.camel", module = "camel-core", version = "2.0.0")
+@Grab(group='joda-time', module='joda-time', version='2.9.4')
+@Grab(group='com.google.guava', module='guava', version='19.0')
+
+import com.google.common.base.Stopwatch
+
+import org.apache.camel.Exchange
+import org.apache.camel.Processor
 import org.apache.camel.impl.DefaultCamelContext
 import org.apache.camel.builder.RouteBuilder
 
+import org.joda.time.DateTime
+
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
+
 class MyRouteBuilder extends RouteBuilder {
+  private def from
+  private def to
+
   void configure() {
-      from("direct://foo").to("mock://result")
+    from(from).to(to)
   }
 }
 
-mrb = new MyRouteBuilder()
-ctx = new DefaultCamelContext()
+class LocalLogger {
+  private def from
+
+  def log(def msg) {
+    println "${new DateTime().toString()}\t${from.toString()}\t[Thread: ${Thread.currentThread().getName()}]\t- ${msg}"
+  }
+}
+
+class QueueProducer {
+  private static final LocalLogger LOG = new LocalLogger(from: QueueProducer.class)
+
+  private def template
+  private String sendTo
+
+  def send(def msg) {
+    LOG.log "Sending: [${msg}]"
+    template.sendBody(sendTo, msg)
+  }
+}
+
+class QueueProcessor implements Processor {
+  private static final LocalLogger LOG = new LocalLogger(from: QueueProcessor.class)
+//  private static final Random RND = new Random(System.currentTimeMillis());
+  private static final Random RND = new Random(0L);
+
+  private Stopwatch timer
+
+  @Override
+  void process(final Exchange ex) throws Exception {
+    final int delay = 1_000 + (RND.nextInt(40) * 100)   //Sleep between 1 and 5 seconds (random incremented by 10ths of a second
+
+    LOG.log "Received: [${ex.toString()}]"
+    Thread.sleep(delay)
+    LOG.log "Finished processing for [${ex.toString()}]. Processing took ${delay/1_000} seconds"
+    LOG.log "Processing took ${timer.elapsed(TimeUnit.MILLISECONDS)} milli-seconds so far"
+  }
+}
+
+final Stopwatch timer = new Stopwatch()
+timer.start()
+
+final int NUM_MESSAGES = 20
+
+final String SEND_Q = "seda://foo"
+final String RECV_Q = "seda://result"
+
+final def logger = new LocalLogger(from: "main")
+
+final def mrb = new MyRouteBuilder(from: SEND_Q, to: RECV_Q)
+final def ctx = new DefaultCamelContext()
 ctx.addRoutes mrb
 ctx.start()
 
-p = ctx.createProducerTemplate()
-p.sendBody "direct:foo", "Camel Ride for beginner"
+final def ep = ctx.getEndpoint(RECV_Q)
 
-e = ctx.getEndpoint("mock://result")
-ex = e.exchanges.first()
-println "INFO> ${ex}"
+final def cons = ep.createConsumer(new QueueProcessor(timer: timer))
+cons.start()
+
+logger.log("Starting producer")
+final def p = new QueueProducer(template: ctx.createProducerTemplate(), sendTo: SEND_Q.replaceAll("/", ""))
+(1..NUM_MESSAGES).forEach { i ->
+  p.send("Call ${i} to queue")
+  Thread.sleep(500)
+}
+
+
+Thread.sleep(60_000)
+
+timer.stop()
+logger.log "Finished. Took ${timer.elapsed(TimeUnit.MILLISECONDS)} milliseconds"
